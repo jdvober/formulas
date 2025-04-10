18f9fea4f8bc69a48ceb38028d8a3ce8603e051a
jvoberMCS <114746137+jvoberMCS@users.noreply.github.com>
2025-04-10 14:29:58 -0400

    Up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
+<script lang="ts">
+	// Add any Javascript Logic Here
+	import World from '../components/World.svelte';
+</script>
 
 <div>Hello</div>
+<World />

95959f8129eea8d89b26e26082f18aeef364f686
jvoberMCS <114746137+jvoberMCS@users.noreply.github.com>
2025-05-02 15:03:25 -0400

    Stuuuuupid Vite?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
-});
+	plugins: [tailwindcss(), sveltekit()],
+	root: 'src',
+	build: {
+		outDir: '../dist',
+		emptyOutDir: true, // also necessary
+	},
+})
